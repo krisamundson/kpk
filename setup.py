83f2213ab12f5f45b90ff07e2703efda7fd16169
Kris Amundson <kamundson@newrelic.com>
2020-05-15 23:33:46 -0700

    Setup
    * keywords
    * console_scripts
    * requirements.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "r") as ld:
+    long_description = ld.read()
+
+with open("requirements.txt") as r:
+    requirements = r.readlines()
 
 setuptools.setup(
     name="kpk",
@@ -13,10 +16,18 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/krisamundson/kpk",
     packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'kpk = kpk.kpk:main'
+        ]
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    keywords="kv key value kvstore",
+    install_requires=requirements,
+    zip_safe=False
 )
