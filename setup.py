17736992618f31ddb0513a4dc01ba6ae9f6cb716
Kris Amundson <kris.amundson@panthalassa.com>
2026-04-13 13:21:47 -0700

    bump app version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as r:
 
 setuptools.setup(
     name="kpk",
-    version="2.3.1",
+    version="2.3.2",
     author="Kris Amundson",
     author_email="krisa@subtend.net",
     description="Simple on-disk key/value store secured by GPG.",
